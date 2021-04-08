8c6ea26fcb81f21f4f7df4f3ac1e839a41c3f607
Kakaranish <stasiekgruz@gmail.com>
2021-04-08 20:10:07 +0200

    + create apply/destroy infra scripts for app_deployment

---
+++ vm_based_deploy_gcp/infra/app_deployment/scripts/ApplyInfra.ps1
@@ -0,0 +1,26 @@
+param(
+  [switch] $Init
+)
+
+Import-Module $PSScriptRoot\..\..\..\scripts\Resolve-EnvPrefix.psm1 -Force
+
+if (-not($env:ASPNETCORE_ENVIRONMENT)) {
+  Write-Error "Environment variable ASPNETCORE_ENVIRONMENT not set" -ErrorAction Stop
+}
+
+$env_prefix = Resolve-EnvPrefix -Environment $env:ASPNETCORE_ENVIRONMENT
+if (-not($env_prefix)) {
+  Write-Error "Invalid environment variable ASPNETCORE_ENVIRONMENT" -ErrorAction Stop
+}
+
+$tf_dir = Resolve-Path "$PSScriptRoot\.."
+
+if ($Init) {
+  terraform.exe -chdir="$tf_dir" init
+}
+
+terraform.exe `
+  -chdir="$tf_dir" `
+  apply `
+  -var="environment=$env:ASPNETCORE_ENVIRONMENT" `
+  -var="environment_prefix=$env_prefix"
---
+++ vm_based_deploy_gcp/infra/app_deployment/scripts/DestroyInfra.ps1
@@ -0,0 +1,18 @@
+Import-Module $PSScriptRoot\..\..\..\scripts\Resolve-EnvPrefix.psm1 -Force
+
+if(-not($env:ASPNETCORE_ENVIRONMENT)) {
+  Write-Error "Environment variable ASPNETCORE_ENVIRONMENT not set" -ErrorAction Stop
+}
+
+$env_prefix = Resolve-EnvPrefix -Environment $env:ASPNETCORE_ENVIRONMENT
+if(-not($env_prefix)) {
+  Write-Error "Invalid environment variable ASPNETCORE_ENVIRONMENT" -ErrorAction Stop
+}
+
+$tf_dir = Resolve-Path "$PSScriptRoot\.."
+
+terraform.exe `
+    -chdir="$tf_dir" `
+    destroy `
+    -var="environment=$env:ASPNETCORE_ENVIRONMENT" `
+    -var="environment_prefix=$env_prefix"
