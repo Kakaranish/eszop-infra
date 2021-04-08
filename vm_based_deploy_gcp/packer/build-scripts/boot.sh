91b78df345ac28f5060618dda13034008b7d1cf2
Kakaranish <stasiekgruz@gmail.com>
2021-04-08 20:25:12 +0200

    +fixed bug with err env var value

--- vm_based_deploy_gcp/packer/build-scripts/boot.sh
+++ vm_based_deploy_gcp/packer/build-scripts/boot.sh
@@ -3,7 +3,7 @@
 keys=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/" -H "Metadata-Flavor: Google")
 for key in ${keys[@]}; do
         val=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/${key}" -H "Metadata-Flavor: Google")
-        export $key=$val > /dev/null 2>&1
+        export $key="${val}" > /dev/null 2>&1
 done
 
 if [ -z "$SERVICE_NAME" ]
