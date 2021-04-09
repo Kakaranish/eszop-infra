dab73a8449c036dea0fadc1682799f97e1b5a203
Kakaranish <stasiekgruz@gmail.com>
2021-04-09 18:25:32 +0200

    + added basic scripts for persistent infra

---
+++ vm_based_deploy_gcp/infra/persistent_resources/scripts/ApplyInfra.ps1
@@ -0,0 +1,13 @@
+param (
+    [switch] $Init
+)
+
+$tf_dir = Resolve-Path "$PSScriptRoot\.."
+
+if ($Init) {
+    terraform.exe -chdir="$tf_dir" init
+}
+
+terraform.exe `
+    -chdir="$tf_dir" `
+    apply
