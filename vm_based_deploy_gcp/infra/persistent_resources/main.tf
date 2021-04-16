0fb72a079de474139c1a8bbe4252bfe2c39c6976
Kakaranish <stasiekgruz@gmail.com>
2021-04-16 15:28:55 +0200

    + added cloud NAT to let MIG machines internet access

--- vm_based_deploy_gcp/infra/persistent_resources/main.tf
+++ vm_based_deploy_gcp/infra/persistent_resources/main.tf
@@ -38,3 +38,17 @@ resource "google_dns_managed_zone" "dns_zone" {
     }
   }
 }
+
+resource "google_compute_router" "cloud_router" {
+  name    = "cloud-router"
+  region  = var.region
+  network = data.google_compute_network.vpc.id
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "cloud-nat"
+  router                             = google_compute_router.cloud_router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
