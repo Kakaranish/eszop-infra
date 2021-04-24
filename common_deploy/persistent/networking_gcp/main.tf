8bf8f6401b60cbce68a95d073e6ccccf36721bc3
Kakaranish <stasiekgruz@gmail.com>
2021-04-24 16:17:50 +0200

    + added gcp networking as persistent common deploy

---
+++ common_deploy/persistent/networking_gcp/main.tf
@@ -0,0 +1,47 @@
+provider "google" {
+  project = var.project_id
+  region  = "europe-central2-a"
+}
+
+data "google_compute_network" "vpc" {
+  name = "default"
+}
+
+resource "google_compute_firewall" "firewall" {
+  name    = "healthcheck-probe"
+  network = data.google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080"]
+  }
+
+  source_ranges = ["35.191.0.0/16", "130.211.0.0/22"]
+}
+
+resource "google_dns_managed_zone" "dns_zone" {
+  name        = "eszop-dns-zone"
+  dns_name    = "eszop."
+  description = "eszop dns name"
+  visibility  = "private"
+
+  private_visibility_config {
+    networks {
+      network_url = data.google_compute_network.vpc.id
+    }
+  }
+}
+
+resource "google_compute_router" "cloud_router" {
+  name    = "cloud-router"
+  region  = var.region
+  network = data.google_compute_network.vpc.id
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "cloud-nat"
+  router                             = google_compute_router.cloud_router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
