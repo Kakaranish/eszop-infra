deee89e04f991d6a66e5595db12c006e069e07c5
Kakaranish <stasiekgruz@gmail.com>
2021-06-03 17:09:21 +0200

    + kustomization

--- docker_based_deploy_azure/scripts/helper-scripts/Unapply-Services.ps1
+++ docker_based_deploy_azure/scripts/helper-scripts/Unapply-Services.ps1
@@ -1,10 +1,9 @@
+param (
+  [Parameter(Mandatory = $true)]
+  [ValidateSet("dev", "staging", "prod")] 
+  [string] $CloudEnv
+)
+
 $kubernetes_dir = Resolve-Path -Path "$PSScriptRoot\..\..\kubernetes"
 
-kubectl delete -f "$kubernetes_dir\services\api-gateway-deploy.yaml"
-kubectl delete -f "$kubernetes_dir\services\offers-deploy.yaml"
-kubectl delete -f "$kubernetes_dir\services\identity-deploy.yaml"
-kubectl delete -f "$kubernetes_dir\services\carts-deploy.yaml"
-kubectl delete -f "$kubernetes_dir\services\orders-deploy.yaml"
-kubectl delete -f "$kubernetes_dir\services\notification-deploy.yaml"
-
-kubectl delete -f "$kubernetes_dir\services\frontend-deploy.yaml"
+kubectl delete -k "${kubernetes_dir}\environments\$CloudEnv"
