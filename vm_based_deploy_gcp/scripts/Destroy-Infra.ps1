c906e1e38741cc118d22e7e80babddc10122fba0
Kakaranish <stasiekgruz@gmail.com>
2021-07-25 20:27:52 +0200

    + destroy infra fix

--- vm_based_deploy_gcp/scripts/Destroy-Infra.ps1
+++ vm_based_deploy_gcp/scripts/Destroy-Infra.ps1
@@ -9,11 +9,19 @@ param (
 $repo_root = "$PSScriptRoot\..\.."
 $tf_dir = Resolve-Path "$PSScriptRoot\.."
 
+Import-Module "$PSScriptRoot\Config.psm1" -Force
 Import-Module "${repo_root}\scripts\Get-AppsConfig.psm1" -Force
 Import-Module "${repo_root}\scripts\Get-InfraConfig.psm1" -Force
 Import-Module "${repo_root}\scripts\Get-InfraConfigOutput.psm1" -Force
 
 # ------------------------------------------------------------------------------
+
+if (-not $gateway_image_name -or -not $offers_image_name -or -not $identity_image_name `
+    -or -not $carts_image_name -or -not $orders_image_name -or -not $notification_image_name `
+    -or -not $frontend_image_name) {
+  Write-Host "[ERROR] Fill Config.psm1 before applying infra" -ForegroundColor Red
+  Exit
+}
 
 $env_prefix_map = @{
   "dev"     = "Staging";
@@ -26,7 +34,7 @@ $infra_global_config = Get-InfraConfig -CloudEnv "global"
 $infra_config = Get-InfraConfig -CloudEnv $CloudEnv
 $infra_output = Get-InfraConfigOutput -CloudEnv $CloudEnv
 
-if ($Init) {
+if ($Init.IsPresent) {
   terraform -chdir="$tf_dir" init
 }
 
@@ -36,7 +44,6 @@ if ($LASTEXITCODE -ne 0) {
 }
 Write-Host "[INFO] Running in '$CloudEnv' terraform workspace" -ForegroundColor Green
 
-$cache_yaml = Get-Content -Path "$PSScriptRoot\output\${CloudEnv}_cache.yaml" | ConvertFrom-Yaml
 terraform `
   -chdir="$tf_dir" `
   destroy `
@@ -44,8 +51,13 @@ terraform `
   -var="global_project_id=$($infra_global_config.GCP_PROJECT_ID)" `
   -var="environment=$($env_prefix_map[$CloudEnv])" `
   -var="environment_prefix=$CloudEnv" `
-  -var="backend_image_name=$($cache_yaml.backend_image_name)" `
-  -var="frontend_image_name=$($cache_yaml.frontend_image_name)" `
+  -var="gateway_image_name=${gateway_image_name}" `
+  -var="offers_image_name=${offers_image_name}" `
+  -var="identity_image_name=${identity_image_name}" `
+  -var="carts_image_name=${carts_image_name}" `
+  -var="orders_image_name=${orders_image_name}" `
+  -var="notification_image_name=${notification_image_name}" `
+  -var="frontend_image_name=${frontend_image_name}" `
   -var="redis_address_ip=$($infra_output.REDIS_ADDRESS)" `
   -var="domain_name=$($infra_config.VM_BASED_DOMAIN_NAME)" `
   -var="sql_server_db_username=$($apps_config.SQLSERVER_USERNAME)" `
